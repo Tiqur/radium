465ea8f69b1116fe60c568c626a6ff310bcdcb6f
Tiqur <Tiqur7@gmail.com>
2022-11-03 16:22:58 -0700

    Made timeframes selectable and updated colors

--- src/components/TopBar/main.js
+++ src/components/TopBar/main.js
@@ -1,17 +1,21 @@
 import styles from './styles.module.scss';
+import { useState } from 'react';
 
 function TopBar() {
+  const [activeTimeframe, setActiveTimeframe] = useState('1m');
+  const timeframes = ['1m', '3m', '15m', '1h', '4h', '1D', '1W'];
+
+  function TimeframeButton(props) {
+    function handleClick() {
+      setActiveTimeframe(props.timeframe);
+    }
+    return <div style={{color: props.timeframe === activeTimeframe ? '#c64be2' : 'default'}}onClick={handleClick}>{props.timeframe}</div>
+  }
+
   return (
     <div className={styles.topBar}>
       <div className={styles.timeframesContainer}>
-        <div>1m</div>
-        <div>3m</div>
-        <div>5m</div>
-        <div>15m</div>
-        <div>1h</div>
-        <div>4h</div>
-        <div>1D</div>
-        <div>1W</div>
+        {timeframes.map((el, i) => <TimeframeButton timeframe={el}/>)}
       </div>
     </div>
   )
